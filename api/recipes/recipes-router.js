c23e9c90caa7396bd092bcba0e920d362e2f6388
Landon Phillips <landonphillips@Landons-MacBook-Pro.local>
2022-05-20 14:06:53 -0500

    wip

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 const Recipe = require('./recipes-model');
 
 router.get("/:recipe_id", (req, res, next) => {
-    Recipe.getRecipeById(req.params.id)
+    Recipe.getRecipeById(req.params.recipe_id)
         .then(result =>{
             res.status(200).json(result)
         })
